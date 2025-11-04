2a41c9be3f49676c56813489ed7d0136346626cc
Griffin Andrew <gaheyrich@gmail.com>
2025-11-04 03:09:43 +0000

    update to use new dev dax devs

--- src/allocator.rs
+++ src/allocator.rs
@@ -37,7 +37,7 @@ unsafe impl GlobalAlloc for HybridGlobal {
             unsafe {
                 INIT.call_once(|| {
                     let dax_size = 118377938944; // PMEM size from ndctl list --namespaces
-                    let dax_path = b"/dev/dax0.1\0".as_ptr() as *const i8; // PMEM path from ndctl list --namespaces
+                    let dax_path = b"/dev/dax0.0\0".as_ptr() as *const i8; // PMEM path from ndctl list --namespaces
                     allocator_bindings::umf_allocator_init(
                         dax_path,
                         dax_size,
@@ -74,7 +74,7 @@ unsafe impl GlobalAlloc for HybridGlobal {
             unsafe {
                 INIT.call_once(|| {
                     let dax_size = 118377938944; // PMEM size from ndctl list --namespaces
-                    let dax_path = b"/dev/dax0.1\0".as_ptr() as *const i8; // PMEM path from ndctl list --namespaces
+                    let dax_path = b"/dev/dax0.0\0".as_ptr() as *const i8; // PMEM path from ndctl list --namespaces
                     allocator_bindings::umf_allocator_init(
                         dax_path,
                         dax_size,
